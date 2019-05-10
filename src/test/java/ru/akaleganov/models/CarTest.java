3dbb176c9c7e9934e0259e5291ceccfd79f35f26
Lis <alexmur07@mail.ru>
2019-05-10 22:29:45 +0700

    Тест авто

--- src/test/java/ru/akaleganov/models/CarTest.java
+++ src/test/java/ru/akaleganov/models/CarTest.java
@@ -1,10 +1,12 @@
 package ru.akaleganov.models;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.*;
@@ -23,7 +25,12 @@ public class CarTest {
     }
 
     @Test
-    public void testCar() {
-        String jsonCar = "{\"id\":\"0\"}";
+    public void testCar() throws IOException {
+        String jsonCar = "{\"marka\":{\"id\":\"1\"}, \"model\":{\"id\":\"1\"}, \"transmission\":{\"id\":\"4\"}, \"yar\":\"1999\"}";
+        Car car = new ObjectMapper().readValue(jsonCar, Car.class);
+        System.out.println(car);
+        testfank(session -> {
+            session.save(car);
+        });
     }
 }
