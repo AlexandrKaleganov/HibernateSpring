79689767b106285806c79c2459f4f8531692c3cb
AlexandrKaleganov <alexmur07@mail.ru>
2019-05-21 15:12:55 +0700

    CarDb + test Car

---
+++ src/main/java/ru/akaleganov/dbmanag/CarDb.java
@@ -0,0 +1,56 @@
+package ru.akaleganov.dbmanag;
+
+import ru.akaleganov.modelsannot.Car;
+
+import java.util.List;
+
+public class CarDb implements Store<Car> {
+    private static final CarDb INSTANCE = new CarDb();
+
+    public static CarDb getInstance() {
+        return INSTANCE;
+    }
+    @Override
+    public Car add(Car car) {
+        error();
+        return null;
+    }
+
+    @Override
+    public Car delete(Car car) {
+        error();
+        return null;
+    }
+
+    @Override
+    public Car edit(Car car) {
+        return openSession(session -> {
+            session.saveOrUpdate(car);
+            return session.load(Car.class, car.getId());
+        });
+    }
+
+    @Override
+    public List<Car> findAll() {
+        error();
+        return null;
+    }
+
+    @Override
+    public Car findByID(Car car) {
+        error();
+        return null;
+    }
+
+    @Override
+    public Car findByName(Car car) {
+        error();
+        return null;
+    }
+
+    @Override
+    public Car findByLogin(Car car) {
+        error();
+        return null;
+    }
+}
---
+++ src/test/java/ru/akaleganov/dbmanag/CarDbTest.java
@@ -0,0 +1,45 @@
+package ru.akaleganov.dbmanag;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+import org.junit.runners.model.Annotatable;
+import ru.akaleganov.modelsannot.Announcement;
+import ru.akaleganov.modelsannot.Car;
+import ru.akaleganov.service.ServiceAddObjects;
+
+import java.util.ArrayList;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.*;
+
+public class CarDbTest {
+    private String jsonAnn = "{\"name\":\"продам машину\", \"author\":{\"id\":\"1\"}}";
+    private String jsonCar = "{\"marka\":{\"id\":\"1\"}, \"model\":{\"id\":\"1\"}, \"transmission\":{\"id\":\"4\"}, \"yar\":\"1999\"}";
+    private Announcement ann = new ServiceAddObjects().addAll(jsonAnn, jsonCar, new ArrayList<>());
+
+    private void testF(Consumer<Session> sessionConsumer) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        sessionConsumer.accept(session);
+        session.getTransaction().commit();
+        session.close();
+        factory.close();
+    }
+
+    @Test
+    public void edit() {
+        testF(session -> session.save(ann));
+        ann.getCar().setDescription("описание объявления");
+        Car expected = CarDb.getInstance().edit(ann.getCar());
+        assertTrue(expected.getDescription().contains("описание объявления"));
+        testF(session -> {
+            Announcement expected2 = session.load(Announcement.class, ann.getId());
+            assertTrue(expected2.getCar().getDescription().contains("описание объявления"));
+            session.delete(expected2);
+        });
+    }
+}
