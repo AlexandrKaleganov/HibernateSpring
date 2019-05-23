1e79420352715da2236e5be338e27e10e4c3ad72
Lis <alexmur07@mail.ru>
2019-05-23 18:18:25 +0700

    Индекс

--- src/main/java/ru/akaleganov/container/ServletIndex.java
+++ src/main/java/ru/akaleganov/container/ServletIndex.java
@@ -17,22 +17,5 @@ public class ServletIndex extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-        String r = "<nav class=\"navbar navbar-expand-sm bg-dark navbar-dark\">\n" +
-                "    <!-- Brand/logo -->\n" +
-                "    <a class=\"navbar-brand\" href=\"${pageContext.servletContext.contextPath}/\">Logo</a>\n" +
-                "    <!-- Links -->\n" +
-                "    <ul class=\"navbar-nav\">\n" +
-                "        <li class=\"nav-item\">\n" +
-                "            <a class=\"nav-link\" href=\"${pageContext.servletContext.contextPath}/create\">Добавить пользователя</a>\n" +
-                "        </li>\n" +
-                "        <li class=\"nav-item\">\n" +
-                "            <a class=\"nav-link\" href=\"${pageContext.servletContext.contextPath}/list\">Список пользователей</a>\n" +
-                "        </li>\n" +
-                "        <li class=\"nav-item\">\n" +
-                "            <a class=\"nav-link\" href=\"${pageContext.servletContext.contextPath}/404\">Подать объявление</a>\n" +
-                "        </li>\n" +
-                "    </ul>\n" +
-                "    <button type=\"submit\" style=\"display: block; margin-left: auto;\" class=\"btn btn-outline-danger\" onclick=\"exit()\">ВЫХОД</button>\n" +
-                "</nav>";
     }
 }
