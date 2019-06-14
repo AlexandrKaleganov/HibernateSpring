f2e90bf65fbf46058d1c49a9e0d0d09cbc37af19
AlexandrKaleganov <alexmur07@mail.ru>
2019-06-14 13:01:33 +0700

    Java Doc магазин причёсан

--- src/main/java/ru/akaleganov/filter/FilterContentType.java
+++ src/main/java/ru/akaleganov/filter/FilterContentType.java
@@ -14,19 +14,19 @@ import java.util.HashMap;
  * @author Alexander Kaleganov
  * @since 13/06/2019
  * фильтр для всех сервлетов
- *
  */
 public class FilterContentType implements Filter {
     private static final Logger LOGGER = Logger.getLogger(FilterContentType.class);
 
     /**
-     *  устанавливает res.setContentType("text/json; charset=windows-1251"), для респонсов
-     *  если  ссылка содержит /todolist или /backet также проверяет, есть ли в текучщей сессии HeshMap
-     *  она отсутствует то создастся новая мапа
-     * @param servletRequest фильтр для сервлетов перехватывает все запросы
-     * @param servletResponse  фильтр для сервлетов перехватывает все запросы
-     * @param filterChain {@link FilterChain}
-     * @throws IOException {@link IOException} исключение может выбросить метод  filterChain.doFilter()
+     * устанавливает res.setContentType("text/json; charset=windows-1251"), для респонсов
+     * если  ссылка содержит /todolist или /backet также проверяет, есть ли в текучщей сессии HeshMap
+     * она отсутствует то создастся новая мапа
+     *
+     * @param servletRequest  фильтр для сервлетов перехватывает все запросы
+     * @param servletResponse фильтр для сервлетов перехватывает все запросы
+     * @param filterChain     {@link FilterChain}
+     * @throws IOException      {@link IOException} исключение может выбросить метод  filterChain.doFilter()
      * @throws ServletException {@link ServletException} исключение может выбросить метод  filterChain.doFilter()
      */
     @Override
